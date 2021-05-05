d0ac20e2db7933c3b273b0ef15bc3f44a6b33e1a
dieeh <diego.paz@hotmail.com>
2021-05-04 23:15:06 -0400

    modified names and created part 2 of hw and header

---
+++ C++ Stuff/tarea1-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "tarea1-2.hpp"
+
+using namespace std;
+
+int main()  {
+    
+
+    return 0;
+}
---
+++ C++ Stuff/tarea1-2.hpp
@@ -0,0 +1,24 @@
+class A {
+private:
+    
+public:
+    A();
+
+    A(unsigned long n);
+
+    A(unsigned long n);
+
+    ~A();
+
+    bool setValue(unsigned long i, int v);
+
+    int getValue(unsigned long i);
+
+    void append(int v);
+
+    void remove();
+
+    unsigned long size();
+};
+
+
