cf282f261b24503660bc7a3c3d205b114e80bd76
dieeh <diego.paz@hotmail.com>
2021-06-07 17:55:32 -0400

    new

---
+++ C++ Stuff/tarea2/tarea2-1.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct tNodo {
+    int contenido;
+    tNodo* sig;
+};
+
+class list {
+    private:
+    tNodo* head;
+    tNodo* curr;
+    tNodo* tail;
+    int lenght;
+    public:
+
+    list() {
+        head=curr=tail= new tNodo;
+        
+    }
+};
