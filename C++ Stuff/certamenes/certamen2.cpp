836d81d973a135513620e743e5e3aac8ee0b810d
dieeh <diego.paz@hotmail.com>
2021-06-22 17:58:28 -0400

    aaaaaaaaaaaaaaaaaa

--- C++ Stuff/certamenes/certamen2.cpp
+++ C++ Stuff/certamenes/certamen2.cpp
@@ -7,10 +7,12 @@ tLista filtrarPuntos(tLista L, Punto supIzq, Punto infDer){
     tLista aux;
     L.moveToStart();
     for (int i = 0; i < L.lenght(); i++){
-        if ( (L.getValue().obtenerX() >= supIzq.obtenerX()) && (L.getValue().obtenerX() <= infDer.obtenerX()) )
-            if ( (L.getValue().obtenerY() <= supIzq.obtenerY()) && (L.getValue().obtenerY() >= infDer.obtenerY()) )
-                aux.append((L.getValue()));
-                L.next();
+        if ( (L.getValue().obtenerX() >= supIzq.obtenerX()) && (L.getValue().obtenerX() <= infDer.obtenerX()) ) {
+            if ( (L.getValue().obtenerY() <= supIzq.obtenerY()) && (L.getValue().obtenerY() >= infDer.obtenerY()) ) {
+                aux.append( L.getValue() );
+                L.next();                
+            }            
+        }
         L.next();
     }
     return aux;
