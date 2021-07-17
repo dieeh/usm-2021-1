2b62400ba67bdd71f436b33f21fd80394075eef9
dieeh <diego.paz@hotmail.com>
2021-07-17 03:07:56 -0400

    toy chato papito

--- C++ Stuff/tarea3/tarea3.cpp
+++ C++ Stuff/tarea3/tarea3.cpp
@@ -1,1 +1,10 @@
 // ola
+
+#include "tarea3.hpp"
+
+int main() {
+    ranura HT[M];
+
+
+    return 0;
+}
---
+++ C++ Stuff/tarea3/tarea3.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+#define M 1000000
+#define VACIA -1
+
+typedef int tipoClave;
+
+struct tipoInfo{
+    //aa
+};
+
+struct ranura{
+    tipoClave k;
+    tipoInfo I;
+};
+
