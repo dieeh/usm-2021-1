9ac6bc6d92193b587a6024efa150421f079acca9
dieeh <diego.paz@hotmail.com>
2021-07-18 00:59:35 -0400

    toy curao

--- C++ Stuff/tarea3/tarea3.cpp
+++ C++ Stuff/tarea3/tarea3.cpp
@@ -1,6 +1,15 @@
 // ola
-
+#include <fstream>
+#include <iostream>
 #include "tarea3.hpp"
+
+int h(tipoClave k){
+
+}
+
+int p(tipoClave k, int i){
+    return i*h2(k);
+}
 
 int main() {
     ranura HT[M];
--- C++ Stuff/tarea3/tarea3.hpp
+++ C++ Stuff/tarea3/tarea3.hpp
@@ -11,7 +11,33 @@ struct tipoInfo{
 };
 
 struct ranura{
-    tipoClave k;
-    tipoInfo I;
+    tipoClave clave;
+    tipoInfo info;
 };
 
+int hashInsert(ranura HT[], tipoClave k, tipoInfo I) {
+    int inicio, i;
+    int pos = inicio = h(k);
+    for (i = 1; HT[pos].clave != VACIA && HT[pos].clave != k; i++) {
+        pos = (inicio + p(k, i)) % M;
+    }
+    if (HT[pos].clave == k) {
+        return 0;
+    }else {
+        HT[pos].clave = k;
+        HT[pos].info = I;
+        return 1;
+    }
+}
+
+tipoInfo hashSearch(ranura HT[], tipoClave k){
+    int inicio, i;
+    int pos = inicio = h(k);
+    for (i = 1; HT[pos].clave != VACIA && HT[pos].clave != k; i++) {
+        pos = (inicio + p(k, i)) % M;
+    }
+    if (HT[pos].clave == k) return HT[pos].info;
+    else {
+        return NULL; //arreglar esta wea
+    }
+}
