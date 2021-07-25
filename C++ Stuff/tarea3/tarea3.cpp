d8eba69eb64723c48890b09a02f1e22fd1040c23
ElekisDe <83084698+ElekisDe@users.noreply.github.com>
2021-07-25 19:16:43 -0400

    a

--- C++ Stuff/tarea3/tarea3.cpp
+++ C++ Stuff/tarea3/tarea3.cpp
@@ -1,10 +1,42 @@
 // ola
 #include <fstream>
+#include <cmath>
+#include <string>
 #include <iostream>
 #include "tarea3.hpp"
 
-int h(tipoClave k){
+using namespace std;
 
+int h(tipoClave k){
+    string x = to_string(k);
+    int dig = x.lenght;
+    int mod = dig % 2
+    if (dig <= 6)
+        return k;
+    else {
+        if (mod = 1) {dig += 1;}
+        x = to_string(num);
+        string y;
+        lugar = dig/2;
+        for (i = lugar - 3, i < lugar + 3, i++){
+            int k = 0;
+            y[k] = x[i];
+            k++;
+            }
+        int h = stoi(y);
+        return h;
+    }
+}
+
+int h2(int h){
+    int cod = h;
+    int total = 0;
+    while(cod > 0){
+        int sumar = 0;
+        sumar = cod % 10;
+        total += sumar;
+    }
+    return total;
 }
 
 int p(tipoClave k, int i){
