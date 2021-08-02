bd3444b63fc78adbe339bd3d9c28bbb58547c9d0
ElekisDe <83084698+ElekisDe@users.noreply.github.com>
2021-08-01 21:22:44 -0400

    aa

--- C++ Stuff/tarea3/tarea3.cpp
+++ C++ Stuff/tarea3/tarea3.cpp
@@ -7,22 +7,15 @@
 
 using namespace std;
 #define e 0.692363
-#define M1 1000000
-#define M2 100000
+#define M 1000000
+#define N 100000
 
 bool flag = false;
 
-int h(tipoClave k){
-    if (flag == false){
-        double h = (M2*((e*k)%1));
+int h(tipoClave k, int Z){
+        double h = (Z*((e*k)%1));
         int x = h;
         return x;
-    }
-    else {
-        double h = (M1*((e*k)%1));
-        int x = h;
-        return x;
-    }
 }
 
 int h2(int h){
@@ -41,8 +34,24 @@ int p(tipoClave k, int i){
 }
 
 int main() {
-    ranura HT[M];
-
-
+    int A,B;
+    ranura HT[A];
+    fstream file1;
+    ruta = "productos.dat";
+    fileEmp.open(ruta1, ios::in|ios::binary);
+    if (!fileEmp.is_open()) {
+        cerr << "Error al abrir el archivo '" << ruta << "'" << endl;
+        exit(1);
+    }
+    fileEmp.close();
+    ranura HT[B];
+    fstream file2;
+    ruta2 = "ofertas.dat";
+    fileComp.open(ruta2, ios::in|ios::binary);
+    if (!fileComp.is_open()) {
+        cerr << "Error al abrir el archivo '" << ruta2 << "'" << endl;
+        exit(1);
+    }
+    fileComp.close();  
     return 0;
 }
